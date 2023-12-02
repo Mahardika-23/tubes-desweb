ab5ebbecaddf75c053fdca24ffd77db35be96ca4
Andin <11211003@student.itk.ac.id>
2023-12-02 15:07:13 +0800

    Login and Signup added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
     <nav className="bg-[#96BBC3] border-gray-200 dark:bg-gray-900 shadow-lg">
@@ -137,6 +139,14 @@ const Navbar = () => {
                 >
                 FAVORIT
                 </a>
+            </li>
+            <li>
+                <Link
+                to ="/register"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                SIGN UP
+                </Link>
             </li>
             </ul>
             </div>
